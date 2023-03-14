3fa644b37926aa50f894396b26c19b8f44b6db3a
ola-cupriak <o.cupriak@wp.pl>
2023-03-14 16:44:47 +0100

    Add new pipeline

---
+++ src/predicting_the_formation_of_chromatin_loops_using_genomic_data/pipelines/hiccups_preprocessing/nodes.py
@@ -0,0 +1,22 @@
+from typing import Any, Callable, Dict
+import pandas as pd
+
+
+
+def dict_partitions(partitioned_input: Dict[str, Callable[[], Any]]) -> Dict[str, pd.DataFrame]:
+    """
+    Load all partitions and save them in a dictionary.
+    Args:
+        partitioned_input: A dictionary with partition ids as keys and load functions as values.
+
+    Returns:
+        dictionary with partition ids as keys and pandas DataFrames as values.
+    """
+    result = dict()
+
+    for partition_key, partition_load_func in sorted(partitioned_input.items()):
+        partition_data = partition_load_func()  # load the actual partition data
+        # concat with existing result
+        result[partition_key] = partition_data
+
+    return result
