4c6ef061a2e9cd031851cfdaa08b88919f65401c
ola-cupriak <o.cupriak@wp.pl>
2023-03-15 13:04:20 +0100

    Add utils file

--- src/predicting_the_formation_of_chromatin_loops_using_genomic_data/pipelines/hiccups_preprocessing/nodes.py
+++ src/predicting_the_formation_of_chromatin_loops_using_genomic_data/pipelines/hiccups_preprocessing/nodes.py
@@ -1,26 +1,8 @@
+from predicting_the_formation_of_chromatin_loops_using_genomic_data.utils import _dict_partitions
 from typing import Any, Callable, Dict
 import pandas as pd
 import copy
 
-
-
-def _dict_partitions(partitioned_input: Dict[str, Callable[[], Any]]) -> Dict[str, pd.DataFrame]:
-    """
-    Load all partitions and save them in a dictionary.
-    Args:
-        partitioned_input: A dictionary with partition ids as keys and load functions as values.
-
-    Returns:
-        dictionary with partition ids as keys and pandas DataFrames as values.
-    """
-    result = dict()
-
-    for partition_key, partition_load_func in sorted(partitioned_input.items()):
-        partition_data = partition_load_func()  # load the actual partition data
-        # concat with existing result
-        result[partition_key] = partition_data
-
-    return result
 
 
 def preprocess_hiccups(partitioned_input: Dict[str, Callable[[], Any]]) -> Dict[str, pd.DataFrame]:
