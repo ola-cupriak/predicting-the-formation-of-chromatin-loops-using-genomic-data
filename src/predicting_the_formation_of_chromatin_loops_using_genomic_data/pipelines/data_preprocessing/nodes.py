3956f063a06006368833ca187496a66506a32a81
ola-cupriak <o.cupriak@wp.pl>
2023-03-31 11:51:21 +0200

    Add first version of motifs adding

--- src/predicting_the_formation_of_chromatin_loops_using_genomic_data/pipelines/data_preprocessing/nodes.py
+++ src/predicting_the_formation_of_chromatin_loops_using_genomic_data/pipelines/data_preprocessing/nodes.py
@@ -4,6 +4,13 @@ from typing import Any, Callable, Dict
 import pandas as pd
 import copy
 from tqdm import tqdm
+from Bio import Seq, SeqIO
+from Bio import motifs
+import warnings
+
+warnings.simplefilter(action="ignore")
+
+
 
 def read_hic(partitioned_input: Dict[str, Callable[[], Any]], 
                 cells2names: Dict[str, dict],
@@ -15,7 +22,7 @@ def read_hic(partitioned_input: Dict[str, Callable[[], Any]],
         cells2names: dictionary, template: {'dataset_name': {'file_name': 'cell_type'}}
         dataset_name: name of the dataset to select from cells2names.
     Returns:
-        dictionary with partition ids as keys and pandas DataFrames as values.
+        dictionary with cell types as keys and pandas DataFrames as values.
     """
     dfs_dict = _dict_partitions(partitioned_input)
     cells2names_dataset_dict = cells2names[dataset_name]
@@ -43,7 +50,7 @@ def _concat_dfs(dfs_dict: Dict[str, pd.DataFrame]) -> pd.DataFrame:
     """
     Concatenate a dataframes from a dictionary to one dataframe.
     Args:
-        dictionary with partition ids as keys and pandas DataFrames as values.
+        dictionary with cell types as keys and pandas DataFrames as values.
     Returns:
         concatenated pandas DataFrame.
     """
@@ -57,9 +64,9 @@ def add_labels(dfs_dict: Dict[str, pd.DataFrame]) -> None:
     """
     Add labels to the dataframes depending on the cell type.
     Args:
-        dictionary with partition ids as keys and pandas DataFrames as values.
+        dictionary with cell types as keys and pandas DataFrames as values.
     Returns:
-        dictionary with partition ids as keys and changed pandas DataFrames as values.
+        dictionary with cell types as keys and changed pandas DataFrames as values.
     """
     df = _concat_dfs(dfs_dict)
     for name, cell_df in dfs_dict.items():
@@ -81,7 +88,7 @@ def read_peaks(partitioned_input: Dict[str, Callable[[], Any]],
         cells2names: dictionary, template: {'dataset_name': {'file_name': 'cell_type'}}
         dataset_name: name of the dataset to select from cells2names.
     Returns:
-        dictionary with partition ids as keys and pandas DataFrames as values.
+        dictionary with cell types as keys and pandas DataFrames as values.
     """
     dfs_dict = _dict_partitions(partitioned_input)
     cells2names_dataset_dict = cells2names[dataset_name]
@@ -136,11 +143,11 @@ def count_peaks(main_dfs_dict: Dict[str, Callable[[], Any]], peaks_dfs_dict: Dic
     Count the number of peaks in both regions of each chromatin loop,
     for each dataframe from the main_dfs_dict dictionary.
     Args:
-        main_dfs_dict: dictionary with partition ids as keys and load functions of pandas DataFrames with chromatin loops as values.
-        peaks_dfs_dict: dictionary with partition ids as keys and pandas DataFrames with experiment peaks as values.
+        main_dfs_dict: dictionary with cell types as keys and load functions of pandas DataFrames with chromatin loops as values.
+        peaks_dfs_dict: dictionary with cell types as keys and pandas DataFrames with experiment peaks as values.
         r: radius of the region around the loop center.
     Returns:
-        dictionary with pandas DataFrames with chromatin loops and the numbers of peaks in both regions of each loop for each cell type.
+        dictionary with cell types as keys and pandas DataFrames with chromatin loops and the numbers of peaks in both regions of each loop as values.
     """
     print(f'Adding peaks counts for {experiment}...')
     # if main_dfs_dict values are not DataFrames, load them
@@ -167,7 +174,7 @@ def read_bigWig(partitioned_input: Dict[str, Callable[[], Any]],
         cells2names: dictionary, template: {'dataset_name': {'file_name': 'cell_type'}}
         dataset_name: name of the dataset to select from cells2names.
     Returns:
-        dictionary with partition ids as keys and paths to bigWig files as values.
+        dictionary with cell types as keys and paths to bigWig files as values.
     """
     input_dict = _dict_partitions(partitioned_input)
     cells2names_dataset_dict = cells2names[dataset_name]
@@ -226,7 +233,7 @@ def add_bigWig_data(main_dfs_dict: Dict[str, Callable[[], Any]],
     Count the number of peaks in both regions of each chromatin loop,
     for each dataframe from the main_dfs_dict dictionary.
     Args:
-        main_dfs_dict: dictionary with partition ids as keys and load functions of pandas DataFrames with chromatin loops as values.
+        main_dfs_dict: dictionary with cell types as keys and load functions of pandas DataFrames with chromatin loops as values.
         bigWig_data_dict: dictionary with cell types as keys and pyBigWig objects as values.
         r: radius of the region around the loop center.
     Returns:
@@ -247,4 +254,188 @@ def add_bigWig_data(main_dfs_dict: Dict[str, Callable[[], Any]],
     return main_dfs_dict
 
 
-def
+def _create_one_region_col(df2cols: pd.DataFrame) -> pd.DataFrame:
+    """
+    Combines two columns with regions into one colun and removes duplicate rows.
+    Args:
+        df2cols: pandas DataFrame with two columns with regions.
+    Returns:
+        pandas DataFrame with one column with regions.
+    """
+    df_part1 = df2cols[['chr', 'x']]
+    df_part2 = df2cols[['chr', 'y']]
+    # rename columns
+    df_part1.columns = ['chr', 'region']
+    df_part2.columns = ['chr', 'region']
+    # combine two columns into one
+    df_one_col = pd.concat([df_part1, df_part2], axis=0)
+    # remove duplicate rows
+    df_one_col = df_one_col.drop_duplicates()
+    # sort by chr and region
+    df_one_col = df_one_col.sort_values(by=['chr', 'region'])
+    # reset index
+    df_one_col = df_one_col.reset_index(drop=True)
+
+    return df_one_col
+
+
+def _add_overlapping_peaks_single_df(df_regions: pd.DataFrame, peaks_df: pd.DataFrame, r: int) -> pd.DataFrame:
+    """
+    Add column with vector of overlapping peaks.
+    Args:
+        df_regions: pandas DataFrame with regions.
+        peaks_df: pandas DataFrame with peaks.
+        r: radius of the region around the loop center.
+    Returns:
+        pandas DataFrame with added column with vector of overlapping peaks.
+    """
+    main_df = copy.deepcopy(df_regions)
+    main_df['open_regions'] = [[]]*len(main_df)
+    # group by chromosome
+    main_df_grouped = main_df.groupby('chr')
+    peaks_df_grouped = peaks_df.groupby('chr')
+
+    for main_chr, main_df_chr in tqdm(main_df_grouped):
+        for peaks_chr, peaks_df_chr in peaks_df_grouped:
+            if main_chr == peaks_chr:
+                    for idx, row in main_df_chr.iterrows():
+                            # TO CHANGE !!! - to overlapping peaks
+                            # overlapping_peaks = peaks_df_chr[(peaks_df_chr['start'] >= row[region] - r)&
+                            #                                     (peaks_df_chr['start'] <= row[region] + r)&
+                            #                                     (peaks_df_chr['end'] >= row[region] - r)&
+                            #                                     (peaks_df_chr['end'] <= row[region] + r)]
+                            overlapping_peaks = peaks_df_chr[~(peaks_df_chr['start'] > row['region'] + r)&
+                                                             ~(peaks_df_chr['end'] < row['region'] - r)]
+                            overlapping_peaks['start'] = overlapping_peaks.apply(lambda x: row['region'] - r if x['start'] < row['region'] - r else x['start'], axis=1)
+                            overlapping_peaks['end'] = overlapping_peaks.apply(lambda x: row['region'] + r if x['end'] > row['region'] + r else x['start'], axis=1)
+                            overlapping_peaks['peak'] = tuple(zip(overlapping_peaks['start'], overlapping_peaks['end']))
+
+                            if len(list(overlapping_peaks['peak'])) > 0:
+                                main_df.loc[idx, 'open_regions'] = [list(overlapping_peaks['peak'])]
+                            
+    return main_df
+
+
+def _add_overlapping_peaks(regions_df: pd.DataFrame,
+                          peaks_dfs_dict: Dict[str, pd.DataFrame],
+                          r: int) -> pd.DataFrame:
+    """
+    Add column with vector of overlapping peaks of open chromatin for each dataframe from the main_dfs_dict dictionary.
+    Args:
+        regions_df: pandas DataFrame with regions.
+        peaks_dfs_dict: dictionary with cell types as keys and pandas DataFrames with open chromatin peaks as values.
+        r: radius of the region around the loop center.
+    Returns:
+        dictionary with cell types as keys and pandas DataFrames with regions of anchors and overlapping peaks of open chromatin as values.
+    """
+    print('Adding overlapping peaks of open chromatin...')
+    df_open_chromatin_dict = {}
+    for cell_type, peaks_df in peaks_dfs_dict.items():
+        print(f'...for {cell_type} cell...')
+        # add overlapping peaks
+        df_regions_and_peaks = _add_overlapping_peaks_single_df(regions_df, peaks_df, r)
+        df_open_chromatin_dict[cell_type] = df_regions_and_peaks
+    print('Done!')
+
+    return df_open_chromatin_dict
+    
+
+def _genome2dict(genome_path: str) -> dict:
+    """Get chromosomes dictionary from fasta file.
+    Args:
+        path (str): path to fasta file with genome.
+    Returns:
+        dict: chromosomes dict {chr: sequence}
+    """
+    print('Saving genome to dictionary...')
+    genome = SeqIO.parse(open(genome_path), 'fasta')
+    chromosomes = {}
+    for sequence in genome:
+        desc = sequence.description
+        if 'Primary Assembly' in desc:
+            chr = desc.split('chromosome ')[1][0]
+            chromosomes[chr] = sequence.seq
+
+    return chromosomes
+
+
+def _find_motif_single_df(df_open_chromatin: pd.DataFrame, 
+                          motif,
+                          genome: dict):
+    """
+    Add two columns with counts of forward and backward motif occurrences in the each anchor in open chromatin regions.
+    """
+    motif_name = motif.name.upper()
+    motif_pssm = motif.pssm
+
+    if not f'motif_{motif_name}_f' in df_open_chromatin.columns:
+        df_open_chromatin[f'motif_{motif_name}_f'] = 0
+        df_open_chromatin[f'motif_{motif_name}_b'] = 0
+
+    chromosoms = df_open_chromatin['chr'].unique()
+    for chr in chromosoms:
+        print(f'...for {chr}...')
+        # read specific chromosome from fasta file
+        sequence = genome[chr]
+
+        df_one_chr = df_open_chromatin[df_open_chromatin['chr'] == chr]
+
+        
+        for idx, row in df_one_chr.iterrows():
+            regions = row['open_regions']
+            for region in regions:
+                start, end = region
+                seq_to_search = sequence[start:end+1]
+                for position, _ in motif_pssm.search(seq_to_search):
+                    if position >= 0:
+                        df_open_chromatin.loc[idx, f'motif_{motif_name}_f'] += 1
+                for position, _ in motif_pssm.search(seq_to_search[::-1]):
+                    if position >= 0:
+                        df_open_chromatin.loc[idx, f'motif_{motif_name}_b'] += 1
+    
+    return df_open_chromatin
+
+
+
+def find_all_motifs(main_dfs_dict: Dict[str, Callable[[], Any]],
+                    peaks_dfs_dict: Dict[str, pd.DataFrame],
+                    motifs_path: str,
+                    genome_path: str,
+                    r: int) -> pd.DataFrame:
+    """
+    """
+    # if main_dfs_dict values are not DataFrames, load them
+    if not isinstance(list(main_dfs_dict.values())[0], pd.DataFrame):
+        main_dfs_dict = _dict_partitions(main_dfs_dict)
+
+    df2cols = main_dfs_dict[list(main_dfs_dict.keys())[0]]
+    print('Done')
+    df_regions = _create_one_region_col(df2cols)
+    print('Done')
+    df_open_chromatin_dict = _add_overlapping_peaks(df_regions, peaks_dfs_dict, r)
+    print('Done')
+
+    genome = _genome2dict(genome_path)
+    print('Done')
+
+    print('Finding motifs...')
+    motifs_object = motifs.parse(open(motifs_path), 'jaspar')
+    for motif in motifs_object:
+        for cell_type, df in df_open_chromatin_dict.items():
+            print(f'...for {cell_type} cell...')
+            df_open_chromatin_dict[cell_type] = _find_motif_single_df(df, motif, genome)
+    print('Done!')
+
+    return df_open_chromatin_dict
+
+
+def _add_motifs_to_single_df(main_df: pd.DataFrame, motifs_df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Count the number of experiment peaks in both regions of each chromatin loop.
+    Args:
+        main_df: pandas DataFrame with chromatin loops.
+        motifs_df: pandas DataFrame with motifs for specific cell type.
+    Returns:
+        pandas DataFrame with chromatin loops and motifs.
+    """
+    
--- src/predicting_the_formation_of_chromatin_loops_using_genomic_data/pipelines/data_preprocessing/pipeline.py
+++ src/predicting_the_formation_of_chromatin_loops_using_genomic_data/pipelines/data_preprocessing/pipeline.py
@@ -4,7 +4,7 @@ generated using Kedro 0.18.6
 """
 
 from kedro.pipeline import Pipeline, node, pipeline
-from .nodes import read_hic, add_labels, read_peaks, count_peaks, read_bigWig, add_bigWig_data
+from .nodes import read_hic, add_labels, read_peaks, count_peaks, read_bigWig, add_bigWig_data, find_all_motifs
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
@@ -27,46 +27,52 @@ def create_pipeline(**kwargs) -> Pipeline:
                 outputs="readed_DNase_seq_peaks",
                 name="read_DNase_seq_peaks_node",
             ),
+            # node(
+            #     func=read_peaks,
+            #     inputs=["CTCF_ChIP_seq_peaks", "cells2names", "params:CTCF_ChIP-seq_peaks"],
+            #     outputs="readed_CTCF_ChIP_seq_peaks",
+            #     name="read_CTCF_ChIP_seq_peaks_node",
+            # ),
+            # node(
+            #     func=read_bigWig,
+            #     inputs=["DNAse_seq_bigWig", "cells2names", "params:DNase-seq_bigWig"],
+            #     outputs="readed_DNase_seq_bigWig",
+            #     name="read_DNase_seq_bigWig_data_node",
+            # ),node(
+            #     func=read_bigWig,
+            #     inputs=["CTCF_ChIP_seq_bigWig", "cells2names", "params:CTCF_ChIP-seq_bigWig"],
+            #     outputs="readed_CTCF_ChIP_seq_bigWig",
+            #     name="read_CTCF_ChIP_seq_bigWig_data_node",
+            # ),
+            # node(
+            #     func=count_peaks,
+            #     inputs=["concat_label_HiC_loops_anotations", "readed_CTCF_ChIP_seq_peaks", "params:CTCF_ChIP-seq_peaks", "params:radius"],
+            #     outputs="HiC_loops_anotations_with_CTCF_ChIP_seq_peaks",
+            #     name="add_CTCF_ChIP_seq_peaks_node",
+            # ),
+            # node(
+            #     func=count_peaks,
+            #     inputs=["HiC_loops_anotations_with_CTCF_ChIP_seq_peaks", "readed_DNase_seq_peaks", "params:DNase-seq_peaks", "params:radius"],
+            #     outputs="HiC_loops_anotations_with_DNase_seq_peaks",
+            #     name="add_DNase_seq_peaks_node",
+            # ),
+            # node(
+            #     func=add_bigWig_data,
+            #     inputs=["HiC_loops_anotations_with_DNase_seq_peaks", "readed_DNase_seq_bigWig", "params:DNase-seq_bigWig", "params:radius"],
+            #     outputs="HiC_loops_anotations_with_DNase_seq_bigWig_data",
+            #     name="add_DNase_seq_bigWig_data_node",
+            # ),
+            # node(
+            #     func=add_bigWig_data,
+            #     inputs=["HiC_loops_anotations_with_DNase_seq_bigWig_data", "readed_CTCF_ChIP_seq_bigWig", "params:CTCF_ChIP-seq_bigWig", "params:radius"],
+            #     outputs="combined_functional_genomics_data",
+            #     name="add_CTCF_ChIP_seq_bigWig_data_node",
+            # ),
             node(
-                func=read_peaks,
-                inputs=["CTCF_ChIP_seq_peaks", "cells2names", "params:CTCF_ChIP-seq_peaks"],
-                outputs="readed_CTCF_ChIP_seq_peaks",
-                name="read_CTCF_ChIP_seq_peaks_node",
-            ),
-            node(
-                func=read_bigWig,
-                inputs=["DNAse_seq_bigWig", "cells2names", "params:DNase-seq_bigWig"],
-                outputs="readed_DNase_seq_bigWig",
-                name="read_DNase_seq_bigWig_data_node",
-            ),node(
-                func=read_bigWig,
-                inputs=["CTCF_ChIP_seq_bigWig", "cells2names", "params:CTCF_ChIP-seq_bigWig"],
-                outputs="readed_CTCF_ChIP_seq_bigWig",
-                name="read_CTCF_ChIP_seq_bigWig_data_node",
-            ),
-            node(
-                func=count_peaks,
-                inputs=["concat_label_HiC_loops_anotations", "readed_CTCF_ChIP_seq_peaks", "params:CTCF_ChIP-seq_peaks", "params:radius"],
-                outputs="HiC_loops_anotations_with_CTCF_ChIP_seq_peaks",
-                name="add_CTCF_ChIP_seq_peaks_node",
-            ),
-            node(
-                func=count_peaks,
-                inputs=["HiC_loops_anotations_with_CTCF_ChIP_seq_peaks", "readed_DNase_seq_peaks", "params:DNase-seq_peaks", "params:radius"],
-                outputs="HiC_loops_anotations_with_DNase_seq_peaks",
-                name="add_DNase_seq_peaks_node",
-            ),
-            node(
-                func=add_bigWig_data,
-                inputs=["HiC_loops_anotations_with_DNase_seq_peaks", "readed_DNase_seq_bigWig", "params:DNase-seq_bigWig", "params:radius"],
-                outputs="HiC_loops_anotations_with_DNase_seq_bigWig_data",
-                name="add_DNase_seq_bigWig_data_node",
-            ),
-            node(
-                func=add_bigWig_data,
-                inputs=["HiC_loops_anotations_with_DNase_seq_bigWig_data", "readed_CTCF_ChIP_seq_bigWig", "params:CTCF_ChIP-seq_bigWig", "params:radius"],
-                outputs="combined_functional_genomics_data",
-                name="add_CTCF_ChIP_seq_bigWig_data_node",
+                func=find_all_motifs,
+                inputs=["concat_label_HiC_loops_anotations", "readed_DNase_seq_peaks", 'motifs_JASPAR_vertebrates', 'hg19', "params:radius"],
+                outputs="motifs_found",
+                name="find_motifs_node",
             ),
         ]
     )
