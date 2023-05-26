9e5437134a62c8f484600cbd65b940e57abf200e
ola-cupriak <o.cupriak@wp.pl>
2023-05-26 14:21:08 +0200

    Add new version of negative sampling (n4)

--- src/predicting_the_formation_of_chromatin_loops_using_genomic_data/pipeline_registry.py
+++ src/predicting_the_formation_of_chromatin_loops_using_genomic_data/pipeline_registry.py
@@ -17,8 +17,9 @@ def register_pipelines() -> Dict[str, Pipeline]:
     data_preprocessing_n1_pipeline = data_preprocessing.create_pipeline(neg_sampling_type='n1')
     data_preprocessing_n2_pipeline = data_preprocessing.create_pipeline(neg_sampling_type='n2')
     data_preprocessing_n3_pipeline = data_preprocessing.create_pipeline(neg_sampling_type='n3')
-    model_training_within_pipeline = model_training.create_pipeline(type='within')
-    model_training_across_pipeline = model_training.create_pipeline(type='across')
+    data_preprocessing_n4_pipeline = data_preprocessing.create_pipeline(neg_sampling_type='n4')
+    model_training_within_pipeline = model_training.create_pipeline(mtype='within')
+    model_training_across_pipeline = model_training.create_pipeline(mtype='across')
 
     return {
         "__default__": data_downloading_pipeline + data_preprocessing_n1_pipeline + model_training_within_pipeline,
@@ -26,6 +27,7 @@ def register_pipelines() -> Dict[str, Pipeline]:
         "data_preprocessing_n1": data_preprocessing_n1_pipeline,
         "data_preprocessing_n2": data_preprocessing_n2_pipeline,
         "data_preprocessing_n3": data_preprocessing_n3_pipeline,
+        "data_preprocessing_n4": data_preprocessing_n4_pipeline,
         "model_training_across_cells": model_training_across_pipeline,
         "model_training_within_cells": model_training_within_pipeline,    
     }
