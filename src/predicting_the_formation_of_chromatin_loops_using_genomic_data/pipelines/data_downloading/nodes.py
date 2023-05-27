a9a0b05bb6b7c4f639c8492f73e0aafc77a7e9de
ola-cupriak <o.cupriak@wp.pl>
2023-05-27 14:30:16 +0200

    Add bigwig file mapping from dm3 to dm6 genome

--- src/predicting_the_formation_of_chromatin_loops_using_genomic_data/pipelines/data_downloading/nodes.py
+++ src/predicting_the_formation_of_chromatin_loops_using_genomic_data/pipelines/data_downloading/nodes.py
@@ -28,7 +28,7 @@ def download_data(datasources_dict: dict) -> str:
     Returns:
         empty (dummy) string
     """
-    to_return = ['None', 'None']
+    to_return = ['None', 'None', {}, {}, 'None']
     for organism, data_dict in datasources_dict.items():
         names_dict = {}
         for experiment, cell_dict in data_dict.items():
@@ -51,18 +51,25 @@ def download_data(datasources_dict: dict) -> str:
                 names_dict[experiment][file_name] = cell_type
                 files_before = files_after
 
-                if organism == 'Homo_sapiens' and experiment == 'reference_genomes' and 'hg19' in cell_dict.keys():
+                if organism == 'Homo_sapiens' and experiment == 'reference_genomes' and cell_type=='hg19':
                     to_return[0] = f'data/01_raw/{organism}/{experiment}/{file_name}'
-                elif organism == 'D_melanogaster' and experiment == 'reference_genomes' and 'dm6' in cell_dict.keys():
-                    to_return[1] = f'data/01_raw/{organism}/{experiment}/{file_name}'
+                if organism == 'D_melanogaster' and experiment == 'reference_genomes' and cell_type=='dm6':
+                    to_return[1] = f'data/01_raw/{organism}/{experiment}/{file_name}' 
+                if organism == 'D_melanogaster' and experiment == 'reference_genomes' and cell_type=='dm3todm6_chain':
+                    to_return[4] = f'data/01_raw/{organism}/{experiment}/{file_name}'
             # gunzip
             if gunzip:
                 subprocess.run(f'gunzip {" ".join(list(files_after))}', shell=True)
             # change directory to the experiment directory
             os.chdir('../../../..')
         yaml_string=yaml.dump(names_dict)
-        with open(f'data/01_raw/{organism}/cells2names.yml', 'w') as f:
-            f.write(yaml_string)
+        # with open(f'data/01_raw/{organism}/cells2names.yml', 'w') as f:
+        #     f.write(yaml_string)
+        if organism == 'Homo_sapiens':
+            to_return[2] = names_dict
+        elif organism == 'D_melanogaster':
+            to_return[3] = names_dict
+
 
     return to_return
 
@@ -106,3 +113,30 @@ def simplify_flies_genome_file(path: str) -> list:
             chromosomes.append(SeqRecord(sequence.seq, id=chrom, description=''))
     
     return chromosomes
+
+
+def map_dm3_to_dm6(cells2names: dict, dm3todm6_mapping: dict, path_chain: str) -> None:
+    """
+    Maps bigwig files from dm3 to dm6 using CrossMap.py
+    Args:
+        cells2names: dictionary with the names of the files
+        dm3todm6_mapping: dictionary with the files to map
+        path_chain: path to the chain file
+    """
+    for experiment, cell_list in dm3todm6_mapping.items():
+        if experiment not in cells2names.keys():
+            print(f'WARNING! No data for {experiment} to map.')
+            continue
+        filenames = cells2names[experiment]
+        filenames = {v: k for k, v in filenames.items()}
+        for cell in cell_list:
+            filename = filenames[cell]
+            path = f'data/01_raw/D_melanogaster/{experiment}/{filename}'
+            subprocess.run(f'CrossMap.py bigwig {path_chain} {path} {path}.mapped', shell=True)
+            os.remove(path)
+            os.rename(f'{path}.mapped.bw', path)
+            to_rm = path.split('/')[:-1]
+            to_rm = '/'.join(to_rm)
+            subprocess.run(f'rm {to_rm}/*.bgr', shell=True)
+            print(f'{experiment} for cell {cell} mapped successfully.\n')
+
