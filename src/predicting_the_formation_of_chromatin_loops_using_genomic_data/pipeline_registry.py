b7e35216535e667d257cffd94f75adc569f94d7b
ola-cupriak <o.cupriak@wp.pl>
2023-06-13 21:25:27 +0200

    Add fly_model_training pipeline

--- src/predicting_the_formation_of_chromatin_loops_using_genomic_data/pipeline_registry.py
+++ src/predicting_the_formation_of_chromatin_loops_using_genomic_data/pipeline_registry.py
@@ -5,6 +5,8 @@ from predicting_the_formation_of_chromatin_loops_using_genomic_data.pipelines im
 from predicting_the_formation_of_chromatin_loops_using_genomic_data.pipelines import data_preprocessing
 from predicting_the_formation_of_chromatin_loops_using_genomic_data.pipelines import fly_data_preprocessing
 from predicting_the_formation_of_chromatin_loops_using_genomic_data.pipelines import model_training
+from predicting_the_formation_of_chromatin_loops_using_genomic_data.pipelines import fly_model_training
+
 from kedro.pipeline import Pipeline
 
 
@@ -27,6 +29,8 @@ def register_pipelines() -> Dict[str, Pipeline]:
     model_training_within_pipeline = model_training.create_pipeline(mtype='within')
     model_training_across_pipeline = model_training.create_pipeline(mtype='across')
 
+    fly_model_training_pipeline = fly_model_training.create_pipeline()
+
     return {
         "__default__": data_downloading_pipeline + data_preprocessing_n1_pipeline + model_training_within_pipeline,
         "data_downloading": data_downloading_pipeline,
@@ -38,5 +42,6 @@ def register_pipelines() -> Dict[str, Pipeline]:
         "fly_data_preprocessing_n3": fly_data_preprocessing_n3_pipeline,
         "fly_data_preprocessing_n4": fly_data_preprocessing_n4_pipeline,
         "model_training_across_cells": model_training_across_pipeline,
-        "model_training_within_cells": model_training_within_pipeline,    
+        "model_training_within_cells": model_training_within_pipeline, 
+        "fly_model_training": fly_model_training_pipeline,   
     }
